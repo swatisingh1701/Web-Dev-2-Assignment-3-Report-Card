6dfa1fbd7663f965eb104aa843f4052ff79470bd
Swati Singh <swati98207@gmail.com>
2026-04-21 22:57:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Reportcard from './components/Reportcard' // Adjust path based on your folder structure
+
+function App() {
+  return (
+    <div className="App">
+      {/* You can add a Navbar or Header here later */}
+      <Reportcard />
+    </div>
+  )
+}
+
+export default App
